e8482b9af20cb7d44e5cc2b40b8d236f0c0a98ce
yutabe <abe21453@cs.saisoncard.co.jp>
2023-06-13 17:52:07 +0900

    db設定など色々

---
+++ src/routes/events/create/+page.svelte
@@ -0,0 +1,20 @@
+<!-- src/routes/+page.svelte -->
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>User Management</title>
+</svelte:head>
+
+<div class="row flex-center flex">
+	<div class="col-6 form-widget">
+		<h1>イベント作成ページ</h1>
+		<form action="?/create" method="post">
+			<label for="name">イベント名</label>
+			<input type="text" name="name" id="name" required />
+			<label for="description">イベント説明</label>
+			<input type="text" name="description" id="description" required />
+			<button type="submit">作成</button>
+		</form>
+	</div>
+</div>
